c8c49e9ccc6eee5eb8579baad9c7fc29f84155a0
Yuliia Dovbak <yulia.dovbak@gmail.com>
2020-12-05 09:10:45 +0000

    Cleaned up the project

--- src/Customer.java
+++
@@ -1,5 +0,0 @@
-public class Customer {
-    public static void main(String[] args) {
-        System.out.println("Customer entity");
-    }
-}
--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello Git World");
-        System.out.println("Test");
 
-        System.out.println("Jack is landed");
-
-        Person p = new Person("Ray");
-
-        System.out.println(p.getName());
-
-        Person q = new Person("Marcus");
-
-        System.out.println(q.getName());
 
     }
 }
