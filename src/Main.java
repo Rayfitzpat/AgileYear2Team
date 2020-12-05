1a0b2916856ecd312ddf561899923e0d70312692
Yuliia Dovbak <yulia.dovbak@gmail.com>
2020-12-05 09:40:09 +0000

    Added code to Main.class, created Publication.java

--- src/Main.java
+++ src/Main.java
@@ -4,6 +4,7 @@ public class Main {
         int number = 10;
         int doubled = number*2;
 
-
+        int number2 = 7;
+        int j = number2*7;
     }
 }
---
+++ src/Publication.java
@@ -0,0 +1,5 @@
+public class Publication {
+    public static void main(String[] args) {
+        System.out.println("Publication here");
+    }
+}
